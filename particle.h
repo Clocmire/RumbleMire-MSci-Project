eedd3ebc4cf40c3448767f63304b8d6d197734b4
alexle91 <alexanderle@hotmail.co.uk>
2012-10-24 18:23:35 +0200

    24/10

--- particle.h
+++ particle.h
@@ -4,21 +4,60 @@
 class particle
 {
 public: 
-particle(threevector velocity(x,y,z), position(x,y,z)); //PLACEHOLDER
-getposition();
-getvelocity();
-setposition();
-setvelocity();
-getacceleration();
-setacceleration();
-getforce();
-setforce();
+particle(threevector vel, double position[3]);
+double* getposition();
+setposition(int x, y, z);
+threevector getvelocity();
+setvelocity(threevector vel);
+threevector getforce();
+setforce(threevector force);
 
 private:
-itsvelocity(threevector vel(x,y,z));
-itsposition();
-itsacceleration();
-itsforce(threevector force(x,y,z));
+threevector itsvelocity;
+double itsposition[3];
+threevector itsforce;
 };
+
+particle::particle(threevector vel, double position[3])
+{
+ itsvelocity = vel;
+// check if this works - itsposition = position;
+ itsposition[0] = position[0];
+ itsposition[1] = position[1];
+ itsposition[2] = position[2];
+}
+
+particle::double* getposition()
+{
+ return itsposition;
+}
+
+particle::setposition(int x, y, z)
+{
+ itsposition[0] = x;
+ itsposition[1] = y;
+ itsposition[2] = z;
+}
+
+particle::threevector getvelocity()
+{
+ return itsvelocity;
+}
+
+particle::setvelocity(threevector vel)
+{
+ itsvelocity = vel;
+}
+
+particle::threevector getforce()
+{
+ return itsforce;
+}
+
+particle::setforce(threevector force)
+{
+ itsforce = force;
+}
+
  
 #endif
