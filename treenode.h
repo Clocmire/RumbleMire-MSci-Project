7c9bd870a194e8d1fc106603ba51e052efec0113
alexle91 <alexanderle@hotmail.co.uk>
2012-10-24 17:07:06 +0200

    24/10

--- treenode.h
+++ treenode.h
@@ -2,14 +2,49 @@
 #define treenode
 {
 public:
-setcount();
-getcount();
 setcom();
-getcom();
+double* getcom();
+int getlevel();
+int getlabel();
+int& getparent();
+
 
 protected:
 itscom(POSITION); //placeholder
 itscount(int number); //spladp
 itsparent(int &parent treenode); //? Check references etc
+int itslabel;
+int itslevel;
+double itscom[3];
+int& itsparent;
+
 };
+
+treenode::setcom(double x, y, z)
+{
+  itscom[0] = x;
+  itscom[1] = y;
+  itscom[2] = z;
+}
+
+treenode::double* getcom()
+{
+  return itscom;
+}
+
+treenode::int getlevel()
+{
+  return itslevel;
+}
+
+treenode::int getlabel()
+{
+  return itslabel;
+}
+
+treenode::int& getparent()
+{
+  return itsparent;
+}
+
 #endif
